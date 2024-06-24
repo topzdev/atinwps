1e7512809290ca88153306d3b3c87c9103917877
topzdev <christianlugod05@gmail.com>
2024-06-24 20:44:31 +0800

    fix accesibility/ux issue

--- src/app/_components/PhotoEditor.tsx
+++ src/app/_components/PhotoEditor.tsx
@@ -1,207 +1,149 @@
 "use client";
 
-import React, {useRef, useState} from 'react';
-import {Button} from "@/components/ui/button";
-import {Slider} from "@/components/ui/slider";
-import {Switch} from "@/components/ui/switch";
-import {cn} from "@/lib/utils";
-import {useToast} from "@/components/ui/use-toast";
-import Cropper from "react-easy-crop";
-import getCroppedImg from "@/lib/cropImage";
+import React, { useEffect, useRef, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
+import PreviewFrame from "@/app/_components/PreviewFrame";
+import PhotoEditorDialog from "@/app/_components/PhotoEditorDialog";
 
 interface PhotoEditorProps {
-    frame?: string;
-    image?: File | null;
+  frame?: string;
+  image?: File | null;
 }
 
-const ZOOM_LEVEL = 1;
-const MIN_ZOOM = .2;
-const MAX_ZOOM = 2;
-const DEFAULT_CROP = {x: 0, y: 0}
-const DEFAULT_ROTATION = 0;
-const MIN_ROTATE = 0;
-const MAX_ROTATE = 360;
-const ROTATE_STEP = 1;
-const ZOOM_STEP = .1;
-const PhotoEditor: React.FC<PhotoEditorProps> = ({frame = 'atinwpsframe.png'}) => {
-    const {toast} = useToast();
-    const editor = useRef<any>(null);
-    const fileInputRef = useRef<any>(null);
-    const [image, setImage] = useState<string | null>(null);
-    const canvasRef = useRef<any>(null);
-    const [loading, setLoading] = useState(false);
-    const [rounded, setRounded] = useState(false);
-    const [zoomLevel, setZoomLevel] = useState<number>(ZOOM_LEVEL);
-    const [crop, setCrop] = useState(DEFAULT_CROP)
-    const [croppedAreaPixels, setCroppedAreaPixels] = useState(null)
-    const [rotation, setRotation] = useState(DEFAULT_ROTATION)
+const PhotoEditor: React.FC<PhotoEditorProps> = ({
+  frame = "atinwpsframe.png",
+}) => {
+  const { toast } = useToast();
+  const fileInputRef = useRef<any>(null);
+  const [image, setImage] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [dialog, setDialog] = useState(false);
+  const [finalImage, setFinalImage] = useState("");
 
-    const onCropComplete = (croppedArea: any, croppedAreaPixels: any) => {
-        setCroppedAreaPixels(croppedAreaPixels)
+  useEffect(() => {
+    if (image) {
+      setDialog(true);
     }
-    const showCroppedImage = async () => {
+  }, [image, setDialog]);
 
+  const handleDownload = async () => {
+    setLoading(true);
+    const a = document.createElement("a");
+    a.href = finalImage;
+    a.download = "atinwps-" + Date.now() + ".png";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    setLoading(false);
+    toast({
+      title: "Photo Downloaded",
+      description:
+        "Thank you for joining this cause! #AtinAngWestPhilippineSea",
+    });
+  };
+
+  const handleInputClick = (event: React.MouseEvent<any, MouseEvent>) => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
     }
-    const handleDownload = async () => {
-        try {
-            setLoading(true);
-            const croppedImage: any = await getCroppedImg(
-                image as string,
-                croppedAreaPixels,
-                rotation
-            )
-            const img = croppedImage;
+  };
 
-            const canvas = canvasRef.current;
-            const ctx = canvas?.getContext('2d');
-
-            if (ctx) {
-                const mainPhotoUrl = img;
-                const frameImageUrl = frame;
-
-                canvas.width = 1000;
-                canvas.height = 1000;
-
-                const mainPhoto = new Image();
-                mainPhoto.crossOrigin = 'anonymous';
-                mainPhoto.onload = () => {
-                    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-                    if (rounded) {
-                        ctx.beginPath();
-                        ctx.arc(canvas.width / 2, canvas.height / 2, canvas.width / 2, 0, Math.PI * 2, true);
-                        ctx.closePath();
-                        ctx.clip();
-                    }
-
-                    ctx.drawImage(mainPhoto, 0, 0, canvas.width, canvas.height);
-                    const frameImage = new Image();
-                    frameImage.crossOrigin = 'anonymous';
-                    frameImage.onload = () => {
-                        ctx.drawImage(frameImage, 0, 0, canvas.width, canvas.height);
-
-                        const imageDataURL = canvas.toDataURL('image/png');
-                        const a = document.createElement('a');
-                        a.href = imageDataURL;
-                        a.download = 'atinwps-' + Date.now() + '.png';
-                        document.body.appendChild(a);
-                        a.click();
-                        document.body.removeChild(a);
-                        setLoading(false);
-                        toast({
-                            title: 'Photo Downloaded',
-                            description: 'Thank you for joining this cause! #AtinAngWestPhilippineSea'
-                        })
-                    };
-                    frameImage.src = frameImageUrl;
-                };
-                mainPhoto.src = mainPhotoUrl;
-            }
-        } catch (e) {
-            console.error(e)
-        }
-
-    }
-
-    const handleInputClick = (event: React.MouseEvent<any, MouseEvent>) => {
-        if (fileInputRef.current) {
-            fileInputRef.current.click();
-        }
-    };
-
-    const handleFileInputChange = (event: any) => {
-        const file = event.target.files[0];
-        if (file) {
-            const reader = new FileReader();
-            reader.onloadend = () => {
-                resetChanges();
-                setImage(reader.result as string);
-                fileInputRef.current.value = null;  // Clear the file input
-            };
-            reader.readAsDataURL(file);
-        }
-    };
-
-    const resetChanges = () => {
-        setImage(null)
-        setZoomLevel(ZOOM_LEVEL);
-        setRounded(false);
-        setCrop(DEFAULT_CROP);
-        setRotation(DEFAULT_ROTATION);
-    }
-
-    const handleRemove = () => {
+  const handleFileInputChange = (event: any) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
         resetChanges();
+        setImage(reader.result as string);
+        fileInputRef.current.value = null; // Clear the file input
+      };
+      reader.readAsDataURL(file);
     }
+  };
 
-    return (
-        <div className="flex flex-col w-full items-center mt-4">
-            <div
-                className={cn('flex relative w-full h-auto aspect-square border border-border bg-slate-100 mx-auto', rounded ? 'rounded-full overflow-hidden' : '')}
-                onClick={image ? () => {
-                } : handleInputClick}
+  const resetChanges = () => {
+    setImage(null);
+  };
+
+  useEffect(() => {
+    if (!dialog) {
+      resetChanges();
+    }
+  }, [dialog, setDialog]);
+
+  const handleRemove = () => {
+    setFinalImage("");
+    resetChanges();
+  };
+
+  return (
+    <div className="flex flex-col w-full items-center mt-4">
+      <PreviewFrame
+        plain={!!finalImage}
+        frame={!finalImage ? frame : null}
+        onClick={!finalImage ? handleInputClick : () => {}}
+      >
+        {finalImage && (
+          <img
+            alt={"Preview Final Image"}
+            className={"h-full w-full"}
+            src={finalImage}
+          />
+        )}
+      </PreviewFrame>
+
+      <input
+        ref={fileInputRef}
+        type={"file"}
+        style={{ display: "none" }}
+        accept="image/*"
+        onChange={handleFileInputChange}
+      />
+
+      <div className={"flex flex-col w-full gap-y-2 mt-2"}>
+        {!finalImage ? (
+          <Button
+            disabled={loading}
+            loading={loading}
+            size={"lg"}
+            className={"w-full"}
+            onClick={handleInputClick}
+          >
+            Upload Photo
+          </Button>
+        ) : (
+          <>
+            <Button
+              disabled={loading}
+              loading={loading}
+              size={"lg"}
+              className={"w-full"}
+              onClick={handleDownload}
             >
-                <Cropper
-                    objectFit={'cover'}
-                    image={image as string}
-                    rotation={rotation}
-                    crop={crop}
-                    restrictPosition={false}
-                    zoom={zoomLevel}
-                    minZoom={MIN_ZOOM}
-                    maxZoom={MAX_ZOOM}
-                    aspect={1}
-                    onCropChange={setCrop}
-                    onZoomChange={setZoomLevel}
-                    onCropComplete={onCropComplete}
-                    onRotationChange={setRotation}
-                    zoomWithScroll={false}
+              Download Photo
+            </Button>
+            <Button
+              className={"w-full"}
+              size={"lg"}
+              variant={"outlined"}
+              onClick={handleRemove}
+            >
+              Remove Photo
+            </Button>
+          </>
+        )}
+      </div>
 
-                />
-                <img src={frame} alt="Selected Frame"
-                     className="w-full h-full absolute top-0 left-0 pointer-events-none"/>
-            </div>
-            <input ref={fileInputRef} type={'file'}
-                   style={{display: 'none'}}
-                   accept="image/*"
-                   onChange={handleFileInputChange}/>
-
-
-            <div className='flex flex-col mt-2 gap-y-2 w-full'>
-                {!image ? <>
-                        <Button className={'w-full'} size={'lg'} onClick={handleInputClick}>Choose Photo</Button>
-                    </> :
-                    <>
-                        <div className='flex flex-col gap-y-3 mb-4'>
-                            <div className='text-start w-full'>
-                                <label className={'block mb-2'} htmlFor="">Zoom</label>
-                                <Slider disabled={!image} value={[zoomLevel]}
-                                        onValueChange={(val) => setZoomLevel(val[0])}
-                                        min={MIN_ZOOM} max={MAX_ZOOM} step={ZOOM_STEP}></Slider>
-                            </div>
-                            <div className='text-start w-full'>
-                                <label className={'block mb-2'} htmlFor="">Rotate</label>
-                                <Slider disabled={!image} value={[rotation]}
-                                        onValueChange={(val) => setRotation(val[0])}
-                                        min={MIN_ROTATE} max={MAX_ROTATE} step={ROTATE_STEP}></Slider>
-                            </div>
-                            <div className='text-start flex items-center gap-x-2 w-full'>
-                                <label className={'block'} htmlFor="">Rounded Frame?</label>
-                                <Switch checked={rounded} onCheckedChange={setRounded}></Switch>
-                            </div>
-
-                        </div>
-                        <Button disabled={loading} loading={loading} size={'lg'} className={'w-full'}
-                                onClick={handleDownload}>Download Photo</Button>
-                        <Button className={'w-full'} size={'lg'} variant={'outlined'} onClick={handleRemove}>Remove
-                            Photo</Button>
-                    </>}
-            </div>
-
-            <canvas ref={canvasRef}
-                    className="mt-4 border border-gray-500 rounded-lg shadow-md h-[500px] w-[500px] hidden"/>
-        </div>
-    );
+      <PhotoEditorDialog
+        dialog={dialog}
+        setDialog={setDialog}
+        frame={frame}
+        image={image}
+        setFinalImage={setFinalImage}
+      ></PhotoEditorDialog>
+    </div>
+  );
 };
 
 export default PhotoEditor;
